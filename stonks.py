9c2691898697b7a6225661549e4dde91ad0afba7
amber1o <100294813+amber1o@users.noreply.github.com>
2022-07-25 14:45:28 -0400

    Update stonks.py

--- stonks.py
+++ stonks.py
@@ -68,68 +68,34 @@ profit_s = [4, 4, 5, 5, 4, 4] <- maximum from this list is 5, therefore answer i
 
 class Solution:
     def stonks(self, prices):
-        
-        # Consider the following: Since there are two transactions possible, there will be two
-        # "windows" of opportunity for Paul to buy and sell stock. These two windows must be from
-        # [0->k], and from [k+1->len(prices)]. The maximum profit will be the the profit from the
-        # first window added to the second window. To solve for the maximum profit in this problem
-        # statement, we would therefore need to establish the two sliding windows, find the maximum
-        # profit from each window, then add them together. 
+        min_price_1 = 100000 
+        profit_1 = []  
+        max_profit_1 = 0 
 
-        # TODO: Analyze first window (forwards profit)
-        min_price_1 = 100000 # Set min price to max range for prices: 10^5, adjustible
-        profit_1 = []  # Set up profit margin in the first window
-        max_profit_1 = 0 # Current max profit is 0
-
-        # Traverse through prices in the forward direction to find profits using x as pointer
-        for x in prices: # x represents time (hours in a day)
+        for x in prices: 
             if min_price_1 > x:
-                # If x is less than the previous min, assign a new minimum to current value x
                 min_price_1 = x
             else:
-                # Otherwise, if x is not a new minimum, adjust the maximum profit variable
-                # The max profit variable ONLY CHANGES when the current profit: x - minimum price
-                # is greater than the previous profit.
+               
                 max_profit_1 = max(max_profit_1, x - min_price_1)
 
-            # Append the profit found to the first list. At the end of the algorithm, the profit_1 list will
-            # contain the maximum profits found through all hours of the day (all items in the prices list) 
             profit_1.append(max_profit_1)
 
-        # TODO: Analyze second window (backwards profit)
-        max_price_2 = 0 # Set max price to min range for prices: 0, adjustible
-        profit_2 = [0]*len(prices) # Set up profit margin in the second window, moving backwards cannot use append
-        max_profit_2 = 0 # Current max profit is 0
+        max_price_2 = 0 
+        profit_2 = [0]*len(prices) 
+        max_profit_2 = 0
 
-        # Traverse through prices in the backwards direction
-        for i in range(len(prices) - 1, -1, -1): # Iterate over len(prices) -> 0 inclusive, decrement by 1
-            x = prices[i] # Set a seperate variable "x" equal to the contents of prices, counting backwards
+        for i in range(len(prices) - 1, -1, -1): 
+            x = prices[i]
             if x > max_price_2: 
-                # If the current pointer x is greater than maximum price, set max price equal to current pointer x
+               
                 max_price_2 =  x
             else:
-                # If the current pointer x is less than maximum price, then there is an opportunity to make profit.
-                # Calcaulte the profit at the current location by subtracting x by max_price_2. (x is smaller!)
-                # If the profit calculated is greater than the maximum profit, assign the new value to max_profit,
-                # Otherwise, the maximum profit stays the same.
                 max_profit_2 = max(max_profit_2, max_price_2 - x)
 
-            # (Similar to append but backwards) Insert the maximum profit available at this hour into the profit_2
-            # list (second window) and repeat for all items in the list
             profit_2[i] = max_profit_2
 
-        # TODO: Calculate max profit from the two windows
-        max_profit = 0 # Default variable
-        for i in range(len(prices)): # Iterate across all items in the list
-            # Maximum profit at given time is equivalent to the maximum profit calculated from each window summed together
-            sum_profit = profit_1[i] + profit_2[i]
-            if sum_profit > max_profit: # If the current sum is greater than the maximum profit, assign max profit to the current sum
-                max_profit = sum_profit
-        
-        print(profit_1)
-        print(profit_2)
-
-        return max_profit # Return solution
+        max_profit = 0 
 
 def main():
     array = input().split(" ")
