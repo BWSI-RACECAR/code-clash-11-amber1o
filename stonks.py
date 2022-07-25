fc4c380b61cbe02b3538d6d3aa2f6792cd825634
amber1o <100294813+amber1o@users.noreply.github.com>
2022-07-25 13:38:38 -0400

    Update stonks.py

--- stonks.py
+++ stonks.py
@@ -34,11 +34,50 @@ Input: [1, 3, 3, 5, 4, 0, 3, 8, 5, 5], Output: 12
 Buy during hour 1 (price = 1), sell during hour 4 (price = 5), net profit = 5 - 1 = 4. 
 Then, buy during hour 6 (price = 0), sell during hour 8 (price = 8), net profit = 8 - 0 = 8. 
 Total profit = 4 + 8 = 12.
+
+
+Demo: [7, 1, 5, 3, 4, 0], Output: 5
+Buy during hour 2 (price = 1), sell during hour 3 (price = 5), net profit = 5 - 1 = 4.
+Then, buy during hour 4 (price = 3), sell during hour 5 (price = 4), net profit = 4 - 3 = 1
+Total profit = 4 + 1 = 5
+
+What the code does:
+
+----------------------------------- Window #1 -----------------------------------
+Iterator        x               min_price_1     x - min_price_1     max_profit_1
+0               7               7               7 - 7 = 0           0               
+1               1               1               1 - 1 = 0           0
+2               5               1               5 - 1 = 4           4
+3               3               1               3 - 1 = 2 [X]       4
+4               4               1               4 - 1 = 3 [X]       4
+5               0               0               0 - 0 = 0 [X]       4
+
+----------------------------------- Window #2 -----------------------------------
+Iterator        x               max_price_2     max_price_2 - x     max_profit_2
+5               0               0               0 - 0 = 0           0               
+4               4               4               4 - 4 = 0           0
+3               3               4               4 - 3 = 1           1
+2               5               5               5 - 5 = 0 [X]       1
+1               1               5               5 - 1 = 4           4
+0               7               7               7 - 7 = 0 [X]       4
+
+profit_1 = [0, 0, 4, 4, 4, 4]
+profit_2 = [4, 4, 1, 1, 0, 0]
+profit_s = [4, 4, 5, 5, 4, 4] <- maximum from this list is 5, therefore answer is 5
 """
 
 class Solution:
     def stonks(self, prices):
-         min_price_1 = 100000 # Set min price to max range for prices: 10^5, adjustible
+        
+        # Consider the following: Since there are two transactions possible, there will be two
+        # "windows" of opportunity for Paul to buy and sell stock. These two windows must be from
+        # [0->k], and from [k+1->len(prices)]. The maximum profit will be the the profit from the
+        # first window added to the second window. To solve for the maximum profit in this problem
+        # statement, we would therefore need to establish the two sliding windows, find the maximum
+        # profit from each window, then add them together. 
+
+        # TODO: Analyze first window (forwards profit)
+        min_price_1 = 100000 # Set min price to max range for prices: 10^5, adjustible
         profit_1 = []  # Set up profit margin in the first window
         max_profit_1 = 0 # Current max profit is 0
 
@@ -90,7 +129,16 @@ class Solution:
         print(profit_1)
         print(profit_2)
 
-        return max_profit
+        return max_profit # Return solution
+
+def main():
+    array = input().split(" ")
+    for x in range (0, len(array)):
+        array[x] = int(array[x])
+
+    tc1 = Solution()
+    ans = tc1.stonks(array)
+    print(ans)
 
 if __name__ == "__main__":
     main()
